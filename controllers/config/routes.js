c99801a5d7ab42f1027889a526664a03380b16b0
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-08-15 16:44:46 +0100

    Conflicts resolved.

--- controllers/config/routes.js
+++ controllers/config/routes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 const happeningsController = require('../controllers/happenings')
+const usersController = require('../controllers/users')
+// const secureRoute = require('../lib/secureRoute')
+
+// ## Happenings Routes
 
 router.route('/happenings')
   .get(happeningsController.index)
@@ -9,5 +13,16 @@ router.route('/happenings/:id')
   .get(happeningsController.show)
   .put(happeningsController.update)
   .delete(happeningsController.delete)
+
+//## User Routes
+
+router.get('/users', usersController.userIndex)
+router.post('/register', usersController.register)
+router.post('/login', usersController.login)
+
+router.route('/users/:id')
+  .get(usersController.userShow)
+  .push(usersController.userUpdate)
+  .delete(usersController.userDelete)
 
 module.exports = router
