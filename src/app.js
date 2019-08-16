404c564513e199a7823ea117343a836664487d9f
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-08-16 15:59:52 +0100

    Merged with Lana's changes.

--- src/app.js
+++ src/app.js
@@ -5,13 +5,9 @@ import { HashRouter, Route, Switch } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
 // import Home from './components/pages/Home'
-<<<<<<< HEAD
 import Navbar from './components/common/Navbar'
 // import SecureRoute from './components/common/SecureRoute'
-=======
 // import Navbar from './components/common/Navbar'
-import SecureRoute from './components/common/SecureRoute'
->>>>>>> development
 
 // import HappeningIndex from './components/happenings/HappeningIndex'
 // import HappeningShow from './components/happenings/HappeningShow'
@@ -19,15 +15,11 @@ import SecureRoute from './components/common/SecureRoute'
 // import HappeningNew from './components/happenings/HappeningNew'
 
 // import UserIndex from './components/happenings/UserIndex'
-<<<<<<< HEAD
-// import UserShow from './components/happenings/UserShow'
-=======
 import UserShow from './components/users/UserShow'
->>>>>>> development
 // import UserEdit from './components/happenings/UserEdit'
 
-// import Register from './components/auth/Register'
-// import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 // import '@fortawesome/fontawesome-free/js/all.js'
 import 'react-toastify/dist/ReactToastify.css'
@@ -39,25 +31,12 @@ class App extends React.Component {
   render() {
     return (
       <HashRouter>
-<<<<<<< HEAD
         <Navbar />
         <ToastContainer position="bottom-right" hideProgressBar={true} />
-
         <Switch>
-=======
-
-
-        <ToastContainer position="bottom-right" hideProgressBar={true} />
-
-        <Switch>
-
           <Route path="/users/:id" component={UserShow} />
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
-
-
-
->>>>>>> development
         </Switch>
       </HashRouter>
     )
