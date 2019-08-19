b33222aeb1f2bef819acc2df30d9a6652ad9cf0c
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2019-08-19 10:13:45 +0100

    userPage fixes

--- src/components/users/UserShow.js
+++ src/components/users/UserShow.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 import { Link } from 'react-router-dom'
 
-// import Auth from '../../lib/Auth'
+import Auth from '../../lib/Auth'
 // import Comment from '../common/Comment'
 
 class Show extends React.Component {
@@ -11,17 +11,26 @@ class Show extends React.Component {
   constructor() {
     super()
     this.state = {
+
       // formData: {rating: 1, content: ""}
     }
 
     this.handleChange = this.handleChange.bind(this)
     // this.handleSubmit = this.handleSubmit.bind(this)
     this.handleDelete = this.handleDelete.bind(this)
+    this.getHappeningsById = this.getHappeningsById.bind(this)
   }
 
   componentDidMount() {
     axios.get(`/api/users/${this.props.match.params.id}`)
       .then(res => this.setState({ user: res.data }))
+      .then(() => {
+        axios.get('/api/happenings')
+          .then(res => this.setState({ happenings: this.getHappeningsById(res.data) }))
+          .then(() => {
+            this.state.user.happenings.push(this.state.happenings[0])
+          })
+      })
   }
 
   handleChange(e) {
@@ -29,13 +38,24 @@ class Show extends React.Component {
     this.setState({formData})
   }
   handleDelete () {
-    // const token = Auth.getToken()
-    axios.delete(`/api/users/${this.props.match.params.id}`
-    //   , {
-    //   headers: { 'Authorization': `Bearer ${token}`}
-    // }
+    const token = Auth.getToken()
+    axios.delete(`/api/users/${this.props.match.params.id}`, {
+      headers: { 'Authorization': `Bearer ${token}`}}
     )
       .then(() => this.props.history.push('/'))
+  }
+
+  getHappeningsById(happenings) {
+    const listOfHappenings =[]
+    for(let i = 0; i<happenings.length; i++) {
+      const happening = happenings[i]
+      console.log(this.state.user.happenings)
+      if(this.state.user.happenings.includes(happening._id)) {
+        listOfHappenings.push(1)
+      }
+    }
+    // console.log(listOfHappenings)
+    return listOfHappenings
   }
 
   // handleSubmit(e) {
@@ -55,7 +75,7 @@ class Show extends React.Component {
   // }
 
   render() {
-    console.log(this.props.match.params.id)
+    console.log()
     return (
       <section className="section">
         <div className="container">
@@ -63,42 +83,37 @@ class Show extends React.Component {
           {!this.state.user && <h2 className="title is-2">Loading...</h2>}
 
           {this.state.user && <div>
-            <header>
-              <div className="columns">
-                <div className="column">
-                  <h1 className="title is-2">{this.state.user.name}</h1>
-                  <h2 className="title is-4">{this.state.user.city}</h2>
-                </div>
-                <div className="column">
-                  <div className="buttons">
-                    <Link
-                      className="button"
-                      to={`/users/${this.state.user._id}/edit`}
-                    >Edit</Link>
-
-                    <button onClick={this.handleDelete} className="button is-danger">Delete</button>
-                  </div>
-                </div>
-              </div>
-
-            </header>
-            <hr />
-
             <div className="columns">
               <div className="column">
-                <figure className="image is-128x128">
-                  <img className="is-rounded" src={this.state.user.photo} />
-                </figure>
+                <figure className="image" style={{ backgroundImage: `url(${this.state.user.photo})` }}/>
               </div>
-
               <div className="column">
-                <h2>About me:</h2>
+                <h1 className="title is-2">{this.state.user.name}</h1>
+                <h2 className="title is-4">{this.state.user.city}</h2>
+                <hr />
+                <h2 className="subtitle is-4" >About me:</h2>
                 <p>{this.state.user.bio}</p>
-
-
-
-
               </div>
+            </div>
+            {Auth.isAuthenticated() && <div className="buttons">
+              <Link
+                className="button"
+                to={`/users/${this.state.user._id}/edit`}
+              >Edit</Link>
+
+              <button onClick={this.handleDelete} className="button is-danger">Delete</button>
+            </div>}
+            <hr />
+            <div className="container">
+              {this.state.user.categories.map(cat =>
+                <span key={cat}
+                  className={`tag is-${cat.split(' ')[0].toLowerCase()}`}
+                >{cat}</span>
+              )}
+            </div>
+            <hr />
+            <div className="container">
+
             </div>
           </div>}
 
