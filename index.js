dd5357285fef64d7773ac3b8b3c6f6aa54dabd02
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-08-19 10:55:07 +0100

    Setting up for deployment (and possibly matts search page changes.

--- index.js
+++ index.js
@@ -11,20 +11,21 @@ const router = require('./config/routes')
 // const queryHandler = require('./lib/queryHandler')
 const errorHandler = require('./lib/errorHandler')
 const { dbURI } = require('./config/environment')
+const { port, dbURI } = require('./config/enviroment')
+
 const app = express()
 //Not yet using the below
 // app.use(queryHandler)
+mongoose.connect(dbURI, { useNewUrlParser: true })
+// look for static files in the 'dist' folder
+// static files are files like index.html, images, fonts, styles etc...
+app.us(express.static(`${__dirname}/dist`))
 app.use(bodyParser.json())
 
 
 app.use('/api', router)
-
 app.use(errorHandler)
-mongoose.connect(dbURI, { useNewUrlParser: true })
 
-app.listen(4000, () => console.log('Listening to port 4000'))
-
-//Not using the below yet. This is needed once we set up testing
-// module.exports = app
+app.listen(port, () => console.log('Listening to port 4000'))
 
 module.exports = app // Export the app for testing
